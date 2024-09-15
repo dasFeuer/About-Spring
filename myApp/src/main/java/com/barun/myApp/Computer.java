0f6da6a8c31508b9a42df99aabc5f07886e8f894
dasFeuer <barunpanthisharma11@gmail.com>
2024-09-15 13:17:29 +0545

    Code updated

--- myApp/src/main/java/com/barun/myApp/Computer.java
+++ myApp/src/main/java/com/barun/myApp/Computer.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
+@Primary
 public class Computer implements Machine {
     public void compile(){
         System.out.println("Compiling the bug 404.");
